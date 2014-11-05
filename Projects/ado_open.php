66647f67c52dbc8a4df73ce71a250c01e846b1ba
markvassell <mdvy96@mail.missouri.edu>
2014-11-05 15:05:23 -0600

    Database changes
    edited ado

--- Projects/ado_open.php
+++ Projects/ado_open.php
@@ -1,29 +1,15 @@
 <?php
-		/******************Possible Alternative Ways to Connect to the Db (These aren't quite working)**********************************************
-	//A) Connect to MS Access Database USING DSN: ODBC connection (name of DSN, username, password)
-	//$conn = odbc_connect('budget', '', '');
-	
-	//B) Connect to MS Access DSN-less
-	//$conn = odbc_connect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=budget", 'root', '') or die('Connection to MS Access Failed: '.odbc_errormsg() );
-	/***********************************************************************************************************************************************************/
-	/* C) Connect to MS Access DSN-less via adodb5 (ActiveX Data Object Database 5)
-	include('../adodb5/adodb.inc.php'); //First include adodb5 to Configure the Database
-	$db = ADONewConnection('access'); //Create new Microsoft Access Connection using adodb5
-	//$db->debug = true; //Used for Debugging Issues
-    $dsn = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=".realpath("budget.accdb").";"; //Realpath dynamical loads the entire path for the MS Access file
-	$db->Connect($dsn); //connect to the file
-	*/
-	$servername = "localhost";
-	$username = "root";
-	$password = "fapri";
+$servername = "localhost";
+$username = "root";
+$password = "fapri";
 
-	// Creating a Connection to msql
-	$db = mysqli_connect($servername, $username, $password, "fapri");
+// Creating a Connection to msql
+$db = mysqli_connect($servername, $username, $password, "movedb");
 
-	// checks connection
-	if($db -> connect_error){
-		die("Connection failed: " . $db->connect_error);
-	}
+// checks connection
+if($db -> connect_error){
+	die("Connection failed: " . $db->connect_error);
+}
 
-	echo "Connected successfully <br>";
+echo "Connected successfully <br>";
 ?>
